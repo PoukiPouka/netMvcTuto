cbdd1f86a5759f1e6d993dc57661440d3c600f8e
PoukiPouka <lapoucavededofus@gmail.com>
2022-10-14 10:59:03 +0200

    Fix Controller with ID

--- netMvcTuto/Program.cs
+++ netMvcTuto/Program.cs
@@ -12,7 +12,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
